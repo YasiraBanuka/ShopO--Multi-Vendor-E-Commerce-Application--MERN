cf45286d86e37cf7f5b8d08ffee20bfa99d18d64
IT21378270 <it21378270@my.sliit.lk>
2024-01-15 23:55:34 +0530

    backend mongodb connected

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const app = require('./app');
+const connectDatabase = require('./db/Database');
+
+// handling uncaught exceptions
+process.on('uncaughtException', (err) => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down due to uncaught exception');
+});
+
+// config
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+    require('dotenv').config({
+        path: "backend/config/.env"
+    });
+}
+
+// connect to database
+connectDatabase();
+
+// create server
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+// handling unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down the server due to unhandled promise rejection');
+    server.close(() => {
+        process.exit(1);
+    });
+});
