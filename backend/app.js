68a6a857a7686b8f132a53ce763a85b53513f84d
IT21378270 <it21378270@my.sliit.lk>
2024-01-16 12:52:04 +0530

    backend + signup() started

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
+const ccokieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(ccokieParser());
+app.use(cors());
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-    require('dotenv').config({
-        path: "backend/config/.env"
-    });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
+
+// import routes
+const user = require("./controller/user");
+
+app.use("/api/v2/user", user);
+
+// for error handling
+app.use(ErrorHandler);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-const app = require('./app');
-const connectDatabase = require('./db/Database');
+const app = require("./app");
+const connectDatabase = require("./db/Database");
 
 // handling uncaught exceptions
-process.on('uncaughtException', (err) => {
-    console.log(`ERROR: ${err.stack}`);
-    console.log('Shutting down due to uncaught exception');
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
 });
 
 // config
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-    require('dotenv').config({
-        path: "backend/config/.env"
-    });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
 
 // connect to database
@@ -19,14 +19,14 @@ connectDatabase();
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
 // handling unhandled promise rejections
-process.on('unhandledRejection', (err) => {
-    console.log(`ERROR: ${err.stack}`);
-    console.log('Shutting down the server due to unhandled promise rejection');
-    server.close(() => {
-        process.exit(1);
-    });
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down the server due to unhandled promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
